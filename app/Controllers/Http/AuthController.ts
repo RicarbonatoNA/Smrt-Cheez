6ed9dcd94800064e7015fe9500455da2814ba377
Ricarbonato <101375487+RicarbonatoNA@users.noreply.github.com>
2022-07-20 00:18:11 -0500

    pulaso

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -4,24 +4,24 @@ import User from 'App/Models/User'
 
 export default class AuthController {
     public async login({request, auth, response}:HttpContextContract){
-        //try{
+        try{
             const email = request.input('email')
             const password = request.input('password')
             const token = await auth.use('api').attempt(email, password, {expiresIn:'30mins'})
             response.ok ({mensaje:'se logueo correctamente', token:token, estado:true})
-        //} catch{
+        } catch{
             response.unauthorized({mensaje:'Hubo un error', estado:false})
-        //}
+        }
     }
 
     public async register({response, request}:HttpContextContract){
-        //try{
+        try{
             const user = request.all()
             await User.create(user)
             response.ok ({mensaje:'se registró correctamente', estado:true})
-        //}
-        //catch{
+        }
+        catch{
             response.badRequest({mensaje:'Hubo un error', estado:false})
-        //}
+        }
     }
 }
